4573809c86169a24f1af078a1e9f85a11f144711
FrQise <hiryuu8@gmail.com>
2023-10-09 22:29:00 +0200

    Added tries counter

--- 30DaysOfPython/Guess the number/guess_the_number.py
+++ 30DaysOfPython/Guess the number/guess_the_number.py
@@ -1,16 +1,25 @@
 import random
 
-number = random.randrange(100)
+max_range = 100
+number = random.randrange(max_range)
 number_guess = None
+count = 0
 
 while number_guess != number:
-    number_guess = input("Guess a number between 1 and 100 : ")
-    number_guess = int(number_guess)
+    number_guess = input(f"Guess a number between 1 and {max_range} : ")
 
-    if number > number_guess:
-        print("The number you need to guess is higher, please enter another number.")
-    elif number_guess > number:
-        print("The number you need to guess is lower, please enter another number.")
-    else:
-        print(f"Congratulations! You've found the number ! It was : {number}")
+    try:
+        number_guess = int(number_guess)
+        if number > number_guess:
+            count += 1
+            print("The number you need to guess is higher, please enter another number.")
+        elif number_guess > number:
+            count +=1
+            print("The number you need to guess is lower, please enter another number.")
+        else:
+            print(
+                f"Congratulations! You've found the number ! It was : {number} in {count} tries")
+
+    except ValueError:
+        print("Please enter a valid number")
         break
