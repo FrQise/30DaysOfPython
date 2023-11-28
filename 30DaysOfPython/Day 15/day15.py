a821d8c916795f1a2722aaf1d0bf82deec291b46
FrQise <hiryuu8@gmail.com>
2023-11-28 15:58:59 +0100

    Day 15 is over

---
+++ 30DaysOfPython/Day 15/day15.py
@@ -0,0 +1,60 @@
+# 30 Days of Python
+# Day 15
+
+#SyntaxError
+
+#print "hello world"
+#missing () (print("hello world"))
+
+#print("------------------------------------------")
+
+#NameError
+
+#print(age)
+#Age is not defined (age = 20)
+
+#print("------------------------------------------")
+
+#IndexError
+
+#numbers = [1,2,3,4,5]
+#print(numbers[5])
+#5 is the 6th index, so it's out of the range.
+
+#print("------------------------------------------")
+
+#AttributeError
+
+#import math
+
+#print(math.PI)
+#Its math.pi not math.PI
+
+#print("------------------------------------------")
+
+#KeyError
+
+#users = {"name":"John", "Years":250,"Country":"Oriath"}
+#print(County)
+#Typo error, the key County does not exist
+
+#print("------------------------------------------")
+
+#TypeError
+
+#print(4+"3")
+#3 is a string not an integrer so it can't calculate
+
+#print("------------------------------------------")
+
+#ImportError
+
+from math import power
+#power does not exist in the math module
+
+#print("------------------------------------------")
+
+#ZeroDivisionError
+
+#print(1/0)
+#Can't divide by zero
